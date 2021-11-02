8aa38b41f3aeb6194d3b51f37ae339c471bbdc9e
HenriqueDMenezes <henriquebm16@hotmail.com>
2021-11-01 23:11:24 -0300

    Construindo a parte para buscar as informaçoes solicitadas

--- index.js
+++ index.js
@@ -11,5 +11,16 @@ axios(url)
   .then(response=>{
    const html =  response.data
    const $ = cheerio.load(html)
-  })
+   const valores = [];
+   $('.a-box-group',html).each(function(){
+
+     const text = $(this).text()
+     const preco = $(this).find('.a-price a-text-price a-size-medium').attr('a-offscreen')
+      valores.push({
+        text,
+        preco
+      })
+    })
+    console.log(valores)
+  }).catch(err => console.log(err))
 app.listen(PORT,()=>console.log(`server running on PORT ${PORT}`))
