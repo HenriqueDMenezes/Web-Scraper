bc777482d5261b8adcf68e127cb84e7241ced86c
HenriqueDMenezes <henriquebm16@hotmail.com>
2021-11-03 21:36:56 -0300

    Ajusti no Web Scraper

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const PORT = 8000;
 const axios = require('axios');
 const cheerio = require('cheerio');
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
-
-url = 'https://www.amazon.com.br/'
+url = 'https://www.theguardian.com/international'
 
 axios(url)
   .then(response=>{
    const html =  response.data
    const $ = cheerio.load(html)
-   const valores = [];
-   $('.a-box-group',html).each(function(){
+   const artigos = [];
 
-     const text = $(this).text()
-     const preco = $(this).find('.a-price a-text-price a-size-medium').attr('a-offscreen')
-      valores.push({
-        text,
-        preco
+
+   $('.fc-item__title',html).each(function(){
+
+     const title = $(this).text()
+     const url = $(this).find('a').attr('href')
+      artigos.push({
+        title,
+        url
       })
     })
-    console.log(valores)
+    console.log(artigos)
   }).catch(err => console.log(err))
 app.listen(PORT,()=>console.log(`server running on PORT ${PORT}`))
